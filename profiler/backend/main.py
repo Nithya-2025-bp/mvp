abbf04f8250c1569d9764e3470c42b2be39507b3
Nithya Vupparapalli <nithya.vupparapalli@blupantera.com>
2026-05-06 13:21:46 +1000

    backend reqs

--- profiler/backend/main.py
+++ profiler/backend/main.py
@@ -10,13 +10,14 @@ app = FastAPI(title="Data Profiler API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    # allow_origins=[
+    #     "http://localhost:5173",
+    #     "http://127.0.0.1:5173",
+    # ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origins=["*"]
 )
 
 TYPE_DETECTION_SAMPLE_SIZE = 1000
