fcd8c93fe9e7346cc041a44564eec80a0bffe65c
Nithya Vupparapalli <nithya.vupparapalli@blupantera.com>
2026-05-08 11:34:14 +1000

    Remove trailing slash

--- profiler/backend/main.py
+++ profiler/backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://data-profiler-mvp.vercel.app/",
+        "https://data-profiler-mvp.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
